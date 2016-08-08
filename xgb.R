05eca1571166f64ea553bf0df76b3640e7597537
rajiviyer <raju.rgi@gmail.com>
2016-08-08 16:47:29 +0530

    logloss 2.27499
    used dcast and data table

--- xgb.R
+++ xgb.R
@@ -1,45 +1,72 @@
 setwd("C:/Rajiv/Work/Data Science/Kaggle/talkingdata")
-library(readr)
-library(dplyr)
-library(lubridate)
-library(caret)
+library(data.table)
 library(xgboost)
-library(sqldf)
-library(caTools)
 
 holdout <- 0
 crossv <- 1
 
-pbdm <- read_csv("phone_brand_device_model.csv",col_types=list(col_character(),col_character(),col_character()))
-events <- read_csv("events.csv",col_types=list(col_integer(),col_character(),col_datetime(),col_number(),col_number()))
-app_events <- read_csv("app_events.csv",col_types=list(col_integer(),col_number(),col_integer(),col_integer()))
-gat <- read_csv("gender_age_train.csv",col_types=list(col_character(),col_character(),col_integer(),col_character()))
-gate <- read_csv("gender_age_test.csv",col_type=list(col_character()))
+pbdm <- fread("phone_brand_device_model.csv",colClasses=c("character","character","character"))
+events <- fread("events.csv",colClasses=c("character","character","character","numeric","numeric"))
+app_events <- fread("app_events.csv",colClasses=c("character","character","character","character"))
+gatr <- fread("gender_age_train.csv",colClasses=c("character","character","integer","character"))
+gate <- fread("gender_age_test.csv",colClasses=c("character"))
 
 pbdm <- pbdm[!duplicated(pbdm),]
 pbdm <- pbdm[-which(duplicated(pbdm$device_id)),]
-#pbdm <- mutate(pbdm,phone_brand = paste0("brand:",as.numeric(as.factor(phone_brand))),device_model=paste0("model:",as.numeric(as.factor(device_model))))
-pbdm <- mutate(pbdm,phone_brand = as.numeric(as.factor(phone_brand)),device_model=as.numeric(as.factor(device_model)))
 
-##########For cv testing#################
+#pbdm <- pbdm[,":="(phone_brand=as.numeric(as.factor(phone_brand)),
+#				   device_model=as.numeric(as.factor(device_model))
+#				   )]
+
+pbdm <- pbdm[,":="(phone_brand=paste0("brand:",as.numeric(as.factor(phone_brand))),
+				   device_model=paste0("model:",as.numeric(as.factor(device_model)))
+				   )]
+
+app_events <- merge(events,app_events,by="event_id",all=FALSE)
+device_apps <- unique(app_events[,.(device_id,app_id)],by=NULL) 
+device_apps$app_id <- paste0("app:",as.numeric(as.factor(device_apps$app_id)))
+rm(app_events,events)
+gc()
+tst <- as.data.frame(table(device_apps$app_id),stringsAsFactors=FALSE)
+jlevels <- tst[tst$Freq<600,]$Var1
+device_apps[device_apps$app_id %in% jlevels,]$app_id <- "app:Others"
+device_apps <- dcast.data.table(device_apps, device_id ~ app_id, length, value.var="device_id", fill=0)
+
 if(holdout) {
 split <- sample.split(gat$gender,SplitRatio=0.85)
-gat <- gat[split==TRUE,]
+gatr <- gat[split==TRUE,]
 gate <- gat[split==FALSE,]
 }
 
-train <- inner_join(pbdm,gat, by="device_id")
-test <- inner_join(pbdm,gate, by="device_id")
+gatr$age <- NULL
+gatr$gender <- NULL
+gate$group <- NA
+full <- rbind(gatr,gate)
 
-if(holdout) {
-test_lab <- as.numeric(as.factor(test$group))-1
-}
+full <- merge(pbdm,full, by="device_id",all=FALSE)
+dev_phone <- full[,.(device_id,phone_brand)]
+dev_model <- full[,.(device_id,device_model)]
+dev_phone <- dcast.data.table(dev_phone, device_id ~ phone_brand, length, value.var="device_id", fill=0)
+dev_model <- dcast.data.table(dev_model, device_id ~ device_model, length, value.var="device_id", fill=0)
+full <- full[,":=" (phone_brand = NULL, device_model = NULL)]
+full <- merge(full,dev_phone,by="device_id",all=FALSE)
+full <- merge(full,dev_model,by="device_id",all=FALSE)
+full <- merge(full,device_apps,by="device_id",all.x=TRUE)
+rm(dev_phone,dev_model,device_apps)
+gc()
+#num_cols <- names(full)[5:ncol(full)]
+for (col in 3:ncol(full)) set(full, which(is.na(full[[col]])), col, 0)
 
+train <- full[!is.na(full$group)]
 train_id <- train$device_id
 train_lab <- as.numeric(as.factor(train$group))-1
-train <- select(train,c(phone_brand,device_model))
+train <- train[,":=" (device_id = NULL, group = NULL)]
+test <- full[is.na(full$group)]
 test_id <- test$device_id
-test <- select(test,c(phone_brand,device_model))
+test <- test[,":=" (device_id = NULL, group = NULL)]
+
+train <- train[, lapply(.SD, as.numeric)]
+test <- test[, lapply(.SD, as.numeric)]
 
 trainMatrix <- as.matrix(train)
 testMatrix <- as.matrix(test)
@@ -53,25 +80,25 @@ param <- list(booster="gblinear",
               lambda=5,
               lambda_bias=0,
               alpha=2)
+			  
 if (crossv) {
-bst.cv <- xgb.cv(param=param, data = trainMatrix, label=train_lab, nfold = 5, nrounds = 500,early.stop.round=10,maximize=FALSE,verbose=T)
+bst.cv <- xgb.cv(param=param, data = trainMatrix, label=train_lab, nfold = 5, nrounds = 400,early.stop.round=10,maximize=FALSE,verbose=T)
 best <- min(bst.cv$test.mlogloss.mean)
 bestIter <- which(bst.cv$test.mlogloss.mean==best)
 }
 else
 {
-bestIter=500
+bestIter=360
 }
 xgb_mod <- xgboost(param=param, data=trainMatrix, label=train_lab, nrounds=bestIter, verbose=1,print.every.n = 5)
 
 if(holdout) {
 pred <- predict(xgb_mod,testMatrix)
-pred <- t(matrix(pred,nrow=length(unique(train_lab))))
-table(pred,test_lab)
+table(pred>0.5,test_lab)
 }
 
 pred <- predict(xgb_mod,testMatrix)
 pred <- t(matrix(pred,nrow=length(unique(train_lab))))
 my_solution <- data.frame(test_id,pred)
 colnames(my_solution) <- c("device_id","F23-","F24-26","F27-28","F29-32","F33-42","F43+","M22-","M23-26","M27-28","M29-31","M32-38","M39+")
-write.csv(my_solution,file="xgb.csv",row.names=FALSE)
+write.csv(my_solution,file="xgb_dc.csv",row.names=FALSE)
